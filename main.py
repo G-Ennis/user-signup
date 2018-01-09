c0769442a060a87bdd4fa50e4d60ea1ccb2a98b7
Gulcin Ennis <gulcin.ennis@gmail.com>
2018-01-08 20:09:33 -0500

    user signup complete

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+    
+
+@app.route("/signup", methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/signup", methods=['GET', 'POST'])
+def validate_form():
+
+    username = request.form['username'] 
+    username_error = '' 
+    password = request.form['password'] 
+    password_error = ''
+    verify = request.form['verify']
+    verify_error = ''
+    email = request.form['email']
+    email_error = ''  
+
+    if len(username) < 3 or len(username) > 20:
+        username_error = "Username must be between 3 and 20 characters"
+     
+    if ' ' in username:
+        username_error = "Not a valid username"        
+
+    if len(password) < 3 or len(password) > 20:
+        password_error = "Password must be between 3 and 20 characters"
+        password = ''
+
+    if ' ' in password:
+        password_error = "Not a valid password"
+        password = ''
+
+    if verify != password or len(verify) == 0:
+        verify_error = "Passwords do not match"
+        verify = ''
+
+    
+    if len(email) < 3 or len(email) > 20:
+        email_error = "Email must be between 3 and 20 characters"
+    if '.' not in email:
+        email_error = "Not a valid email"
+    if '@' not in email:
+        email_error = "Not a valid email"
+    if ' ' in email:
+        email_error = "Not a valid email"
+                        
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect (url_for("welcome"))
+    else:
+        return render_template("index.html", username = username, username_error = username_error, password_error = password_error, verify_error = verify_error, email = email, email_error = email_error)
+
+
+@app.route('/welcome')
+def welcome():
+    Username = request.args.get('username')
+    return render_template("welcome.html")
+
+app.run()
+
+
+
+
